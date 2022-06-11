1b482e59a4e5a5dff8f799889a820f147263e578
Oreo <thediamondgamer331@outlook.com>
2022-06-11 22:59:34 +0100

    Update leaveguild.js

--- src/commands/leaveguild.js
+++ src/commands/leaveguild.js
@@ -1,9 +1,10 @@
 const { MessageEmbed } = require("discord.js");
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const {Administrators} = require('../config/other')
 
 module.exports = {
-    name: "testcommand", // COMMAND NAME
-    description: `cmddes`, // COMMAND DESCRIPTION
+    name: "leaveguild", // COMMAND NAME
+    description: `Leaves a guild`, // COMMAND DESCRIPTION
     help_menu: {
         display: true, // SHOWS IN HELP COMMAND (ANY MENU)
         devlock: false, // ONLY SHOWS FOR DEVS (?)
@@ -11,8 +12,8 @@ module.exports = {
         category: "other" // THE CATEGORY
     },
     data: new SlashCommandBuilder()
-        .setName('testcommand')
-        .setDescription(`cmddes`)
+        .setName('leaveguild')
+        .setDescription(`Leaves a guild`)
         .addNumberOption(option => 
                          option.setName('guild')
                           .setDescription('The id of the guild to leave')
@@ -22,6 +23,8 @@ module.exports = {
     execute_m(client, message, args) { }, // THE EXECUTION THROUGH MESSAGE
     async execute_s(client, interaction) { // THE EXECUTION THROUGH SLASH COMMAND
       const gid = interaction.options.getNumber('guild');
+      
+      if(!Administrators.includes(interaction.user.id)) return interaction.reply({content: 'You dont have permission to run this command'});
       
       let guild;
       if(gid) guild = client.guilds.cache.get(gid) || await client.guilds.fetch(gid).catch(console.log);
